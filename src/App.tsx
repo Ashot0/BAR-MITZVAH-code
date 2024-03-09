86a60fae5d43e13bffe311779219be26d284b709
Ashot0 <illia.holovan@gmail.com>
2024-03-09 15:30:11 +0200

    Finaly

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
-import AboutPage from './pages/AboutPage/AboutPage';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="*" element={<HomePage />} />
-			<Route path="/about" element={<AboutPage />} />
 		</Routes>
 	);
 }
